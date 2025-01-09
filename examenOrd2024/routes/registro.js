054c1a55c2ee9dafdbf9ec523fdccc6b1fedad15
xlenyit <elencari@ucm.es>
2025-01-09 16:17:08 +0100

    trabajando en ej 3 registro

--- examenOrd2024/routes/registro.js
+++ examenOrd2024/routes/registro.js
@@ -2,29 +2,47 @@ var express = require('express');
 var router = express.Router();
 var multer = require('multer')
 
+// var validateForm = (req,res,next)=>{
+//     const {nombre, apellidos, correo, contrasena} = req.body;
+    
+//     else {
+//         console.log("Llego al else vale supongo")
+//         next();
+//     }
+//     // next();
+// }
 router.get("/", (req,res)=>{
     res.render("registro" )
 })
-var validateForm = (req,res,next)=>{
+
+router.post("/register",(req,res,next)=>{
+    console.log("Llego a tu corazoncito")
     const {nombre, apellidos, correo, contrasena} = req.body;
+    console.log(nombre, apellidos, correo, contrasena)
+    console.log(req.body)
+    const correoCheck = /@ucm\.es$/;
     if(!nombre){
-        console.log("Llego al if ")
         const err = new Error("Nombre es obligatorio");
         err.status = 404; 
         res.status(404);
         // res.render('error', {err})
         next(err)
     }
-    else {
-        console.log("Llego al else vale supongo")
-        next();
+    if(!apellidos){
+        const err = new Error("Apellidos es obligatorio");
+        err.status = 404; 
+        res.status(404);
+        // res.render('error', {err})
+        next(err)
     }
-    // next();
-}
-router.post("/register",validateForm, (req,res)=>{
-    console.log("Llego a tu corazoncito")
-    const {nombre, apellidos, correo, contrasena} = req.body;
-    // req.app.push({nombre, apellidos, correo, contrasena})
+    if(!correoCheck.test(correo)){
+        const err = new Error("El correo debe terminar en @ucm.es");
+        err.status = 404; 
+        res.status(404);
+        // res.render('error', {err})
+        next(err)
+    }
+    req.app.locals.usuariosList.push({nombre, apellidos, correo, contrasena})
     // console.log(usuariosList)
     res.redirect("/usuarios")
 })
